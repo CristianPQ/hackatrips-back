d689a1bfcd9cbe5c722c98affc3a002edae9b36c
jorchg <rushcentro@gmail.com>
2018-01-20 19:34:26 +0100

    Added cors headers

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const restify = require('restify');
 const {mongoose, User, Object}  = require('./models');
 
-
 const server = restify.createServer({
   name: 'hackatrips-back',
   version: '1.0.0'
 });
+
+server.use(
+  function crossOrigin(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    return next();
+  }
+);
 
 server.listen(process.env.PORT || 8000, function () {
   console.log('%s listening at %s', server.name, server.url);
