64f06cb3225bbd0a7f8359a7f5a8e0a53f316c8d
Cristian Pinto <cristian.pinto@sage.com>
2018-01-21 11:17:56 +0100

    Oracle Enabled

--- models/index.js
+++ models/index.js
@@ -2,8 +2,11 @@ const mongoose = require('mongoose');
 const ObjectSchema = require('./object');
 const UserSchema = require('./user');
 
-mongoose.connect('mongodb://test:password@ds211088.mlab.com:11088/hackatrips2018');
-// mongoose.connect('mongodb://test:password@ds211088.mlab.com:11088/testtest');
+//mLab
+// mongoose.connect('mongodb://test:password@ds211088.mlab.com:11088/hackatrips2018');
+
+// Oracle
+mongoose.connect('mongodb://test:password@129.158.75.223:27017/hackatrips');
 
 let Object = mongoose.model('Object', ObjectSchema);
 let User = mongoose.model('User', UserSchema);
